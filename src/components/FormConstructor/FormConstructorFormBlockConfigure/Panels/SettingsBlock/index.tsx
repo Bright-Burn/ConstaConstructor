a00371b10d8edbe826b2b958ae0d0e134820b692
Andrew161644 <truba24032016@mail.ru>
2023-02-23 15:36:25 +0500

    Доработать механизм выбора конкретного элемента

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/SettingsBlock/index.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/SettingsBlock/index.tsx
@@ -1,6 +1,13 @@
-import React, { FC } from 'react'
+import React, { FC, useLayoutEffect } from 'react'
+import { useAppSelector } from '../../../store/formElements'
 import styles from './styles.module.css'
 
 export const Settings: FC = () => {
+  const { selectedElement } = useAppSelector(state => state.formConstructor)
+
+  useLayoutEffect(() => {
+    console.log(selectedElement)
+  }, [selectedElement])
+
   return <div className={`${styles.settingsBlock} borderCard`}></div>
 }
