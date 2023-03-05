a910a73aae452c7203d791a7308262c8f81f52f9
Andrew161644 <truba24032016@mail.ru>
2023-03-05 20:26:41 +0500

    Добавить return

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/DroppableLayer/DroppableLayer.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/DroppableLayer/DroppableLayer.tsx
@@ -111,6 +111,7 @@ export const DroppableLayer: FC<IDroppableLayer> = ({ parentElementId }) => {
     const isBaseComponent = event.dataTransfer.getData('BaseComponent')
     if (isBaseComponent === 'true') {
       handleOnDropBaseComponent()
+      return
     }
 
     if (groupElementType) {
