79d3bfb5f732799e3567794ba4c7177985983e93
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2023-03-16 09:52:51 +0500

    добавить деселект элементов

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/FormBlock/FormBlock.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/FormBlock/FormBlock.tsx
@@ -1,9 +1,27 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import { formConstructorSlice, useAppDispatch, useAppSelector } from '../../../store/formElements'
 import { DroppableLayer } from '../../DroppableLayer'
 import styles from './styles.module.css'
 
 export const FormBlock: FC = () => {
   const [formBlockParentId] = useState<string>('root')
+  const { selectedElement } = useAppSelector(state => state.formConstructor)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const escHandler = (e: KeyboardEvent) => {
+      console.log(e)
+      if (e.key === 'Escape') {
+        console.log(selectedElement)
+        dispatch(
+          formConstructorSlice.actions.setSelectedElement(null),
+        )
+      }
+    }
+    document.body.addEventListener('keydown', escHandler)
+    return () => document.body.removeEventListener('keydown', escHandler)
+  }, [selectedElement])
+
   return (
     <div className={`${styles.formBlock} borderCard`}>
       <div className={`${styles.formBlockContent}`}>
