609245f58a8c9554ddf4414d6c8edc2a3c3b1108
Arthur Khakimov <verybigfolder@yandex.ru>
2023-03-17 23:46:27 +0500

    Убрал рекурсивные импорты

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/index.ts
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/index.ts
@@ -1,19 +1,2 @@
-export {
-  BadgeFormElement,
-  ButtonFormElement,
-  CardFormElement,
-  CardWithBarChart,
-  CheckboxFormElement,
-  HeaderCognitiveGeologist,
-  HeaderWithBreadcrumbs,
-  HeaderWithStatus,
-  InformerFormElement,
-  LayoutFormElement,
-  PlaceholderFormElement,
-  ProjectGrid,
-  TabsFormElement,
-  TextFieldFormElement,
-  TextFormElement,
-} from './Elements'
 export { FormConstructorFormBlockConfigure } from './FormConstructorFormBlockConfigure'
 export { DroppableLayer } from './DroppableLayer'
