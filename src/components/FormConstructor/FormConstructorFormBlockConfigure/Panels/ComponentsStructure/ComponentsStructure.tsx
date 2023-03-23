93046fc18315d9dadc9e5db8f8113fe6b342057d
19Sanji <bakirovdamir19@gmail.com>
2023-03-23 14:24:18 +0500

    Реализовать возможность сворачивать и разворачивать панели настроек и панели компонентов

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/ComponentsStructure/ComponentsStructure.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/ComponentsStructure/ComponentsStructure.tsx
@@ -1,14 +1,18 @@
-import { Tabs } from '@consta/uikit/Tabs'
 import React, { useState } from 'react'
+import styles from './styles.module.css'
+import { ComponentsTabItem } from './types'
 import { BaseComponents } from './BaseComponents'
 import { ComponentItems } from './ComponentItems'
 import { ComponentTree } from './ComponentTree'
 import { componentsTabItems } from './content'
-import styles from './styles.module.css'
-import { ComponentsTabItem } from './types'
+import { Tabs } from '@consta/uikit/Tabs'
+import { IconArrowLeft } from '@consta/uikit/IconArrowLeft'
+import { IconArrowRight } from '@consta/uikit/IconArrowRight'
+import { Button } from '@consta/uikit/Button'
 
-export const ComponentsStructure = () => {
+export const ConponentsStructure = () => {
   const [tabValue, setTabValue] = useState<ComponentsTabItem | null>(componentsTabItems[0])
+  const [isOpened, setIsOpened] = useState<boolean>(true)
 
   const getTabContentRenderer = () => {
     switch (tabValue) {
@@ -19,19 +23,49 @@ export const ComponentsStructure = () => {
     }
   }
 
+  const openAndCloseSetting = () => {
+    setIsOpened(!isOpened)
+  }
+
   return (
-    <div className={`${styles.componentStructure} borderCard`}>
-      <div className={styles.tabs}>
-        <Tabs
-          value={tabValue}
-          onChange={({ value }) => setTabValue(value)}
-          items={componentsTabItems}
-          getItemIcon={(item: ComponentsTabItem) => item.icon}
-          size='xs'
-        />
-      </div>
-      {getTabContentRenderer()}
-      <ComponentTree />
-    </div>
+    <>
+      {isOpened ? (
+        <>
+          <div className={`${styles.componentStructure} borderCard`}>
+            <div className={styles.tabs}>
+              <Tabs
+                value={tabValue}
+                onChange={({ value }) => setTabValue(value)}
+                items={componentsTabItems}
+                getItemIcon={(item: ComponentsTabItem) => item.icon}
+                size='xs'
+              />
+            </div>
+            {getTabContentRenderer()}
+            <ComponentTree />
+          </div>
+          <div className={styles.openAndCloseBatton}>
+            <Button
+              onlyIcon
+              iconLeft={IconArrowLeft}
+              onClick={openAndCloseSetting}
+              view='ghost'
+              size='s'
+            />
+          </div>
+        </>
+      ) : (
+        <div className={styles.openAndCloseBatton}>
+          <Button
+            onlyIcon
+            iconLeft={IconArrowRight}
+            onClick={openAndCloseSetting}
+            view='ghost'
+            size='s'
+          />
+        </div>
+      )}
+    </>
   )
 }
+>>>>>>> fa8337a (Реализовать возможность сворачивать и):src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/ConponentsStructure/ConponentsStructure.tsx
