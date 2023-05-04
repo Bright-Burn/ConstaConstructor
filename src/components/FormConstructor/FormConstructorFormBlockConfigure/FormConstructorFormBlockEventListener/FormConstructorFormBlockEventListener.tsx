b998b986fa81a5817c9c72cb5a65ae079aa45f6c
Губайдуллин Ирек Рафаилович <gubaydullin.ir@gazprom-neft.ru>
2023-05-04 14:25:01 +0500

    fix: Форматировать переключение по горячим клавишам

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/FormConstructorFormBlockEventListener/FormConstructorFormBlockEventListener.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/FormConstructorFormBlockEventListener/FormConstructorFormBlockEventListener.tsx
@@ -1,36 +1,28 @@
-import React, { FC, ReactNode, useEffect } from 'react'
-import {
-  formConstructorSlice,
-  useAppDispatch,
-  useAppSelector,
-} from '../../store/formElements/slices'
+import React, { FC, ReactNode, useCallback, useEffect } from 'react'
+import { formConstructorSlice, useAppDispatch } from '../../store/formElements'
 import css from './styles.module.css'
 
 interface Props {
   children?: ReactNode
 }
+
 export const FormConstructorFormBlockEventListener: FC<Props> = ({ children }) => {
   const dispatch = useAppDispatch()
-  const settingsPanelState = useAppSelector(state => state.formConstructor.settingsPanelState)
-  const componentsStructurePanelState = useAppSelector(
-    state => state.formConstructor.componentsStructurePanelState,
-  )
-
-  const onKeyDown = (e: { code: string; ctrlKey: any }) => {
-    if (e.code === 'Space' && e.ctrlKey) {
-      dispatch(
-        formConstructorSlice.actions.togglePanelsByHotkey({
-          settingsPanelState: !settingsPanelState,
-          componentsStructurePanelState: !componentsStructurePanelState,
-        }),
-      )
+  
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      const { code, ctrlKey } = e
+      if (code === 'Space' && ctrlKey) {
+        dispatch(formConstructorSlice.actions.togglePanelsByHotkey())
+      }
     }
-  }
 
-  return (
-    <div className={css.formConstructerEventListener} onKeyDown={onKeyDown}>
-      {children}
-    </div>
-  )
+    document.addEventListener('keydown', onKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+    }
+  }, [])
+
+  return <div className={css.formConstructorEventListener}>{children}</div>
 }
-
