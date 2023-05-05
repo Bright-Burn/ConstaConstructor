aaa096dfaf53b85f7ac885545ba5423bd6f80c72
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-05-05 13:11:17 +0500

    подправил код

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/DataTimeSettings/DataTimeSettings.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/DataTimeSettings/DataTimeSettings.tsx
@@ -2,7 +2,7 @@ import { Select } from '@consta/uikit/Select'
 import { useItemsHandlers } from './ItemsService'
 import { TextField } from '@consta/uikit/TextField'
 import { typeArray, viewArray } from './types'
-import { DateTimePropType, DateTimePropView, DateTimeProps } from '@consta/uikit/DateTime'
+import { DateTimePropType, DateTimePropView } from '@consta/uikit/DateTime'
 import { DatePicker } from '@consta/uikit/DatePicker'
 
 export const DataTimeSettings = () => {
@@ -41,19 +41,19 @@ export const DataTimeSettings = () => {
         onChange={onChangeMultiplicityHours}
         label='multiplicityHours'
         type='number'
-        value={itemsProps.multiplicityHours}
+        value={`${itemsProps.multiplicityHours}`}
       />
       <TextField
         onChange={onChangeMultiplicityMinutes}
         label='multiplicityMinutes'
         type='number'
-        value={itemsProps.multiplicityMinutes}
+        value={`${itemsProps.multiplicityMinutes}`}
       />
       <TextField
         onChange={onChangeMultiplicitySeconds}
         label='multiplicitySeconds'
         type='number'
-        value={itemsProps.multiplicitySeconds}
+        value={`${itemsProps.multiplicitySeconds}`}
       />
     </>
   )
