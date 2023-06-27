cd6aca1806c9cebf2437526c77a58d7e6dc31830
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-06-27 14:00:35 +0500

    fix: Поменять неработающий код

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/BreadcrumbsSettings/ItemsService.ts
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/BreadcrumbsSettings/ItemsService.ts
@@ -7,7 +7,6 @@ import { BreadcrumbPropFitMode, BreadcrumbPropSize, DefaultItem } from '@consta/
 export const useItemsHandlers = () => {
   const { selectedElementProps, selectedElement } = useAppSelector(state => state.formConstructor)
   const dispatch = useDispatch()
-  const getButtonElementProps = (x: UnionProps): x is BreadcrumbProps => 'label' in x
   const onDispatch = (selectedElement: ISelectedElement, newProps: BreadcrumbProps) => {
     dispatch(
       formConstructorSlice.actions.setSelectedElement({
@@ -20,9 +19,8 @@ export const useItemsHandlers = () => {
 
   const onChangeItemsCount = ({ value }: { value: string | null }) => {
     if (selectedElement && value) {
-      if (selectedElementProps && getButtonElementProps(selectedElementProps)) {
-        const newProps: BreadcrumbProps = {
-          ...selectedElementProps,
+      const newProps: BreadcrumbProps = {
+          ...selectedElementProps as BreadcrumbProps,
         }
 
         let itemsProps = [...newProps.items]
@@ -41,45 +39,38 @@ export const useItemsHandlers = () => {
         newProps.items = itemsProps
         onDispatch(selectedElement, newProps)
       }
-    }
   }
 
   const onChangeItems = (items: DefaultItem[]) => {
     if (selectedElement && items) {
-      if (selectedElementProps && getButtonElementProps(selectedElementProps)) {
         const newProps: BreadcrumbProps = {
-          ...selectedElementProps,
+          ...selectedElementProps as BreadcrumbProps,
         }
 
         newProps.items = [...items]
         onDispatch(selectedElement, newProps)
-      }
     }
   }
 
   const onChangeSize = (value: BreadcrumbPropSize | null) => {
     if (selectedElement && value) {
-      if (selectedElementProps && getButtonElementProps(selectedElementProps)) {
-        const newProps: BreadcrumbProps = {
-          ...selectedElementProps,
+        const newProps : BreadcrumbProps = {
+          ...selectedElementProps as BreadcrumbProps,
         }
 
         newProps.size = value
         onDispatch(selectedElement, newProps)
-      }
     }
   }
 
 const onChangeFitMode = (value: BreadcrumbPropFitMode | null) => {
   if (selectedElement && value) {
-    if (selectedElementProps && getButtonElementProps(selectedElementProps)){
       const newProps: BreadcrumbProps = {
-        ...selectedElementProps ,
+        ...selectedElementProps as BreadcrumbProps,
       }
       
       newProps.fitMode = value
       onDispatch(selectedElement, newProps)
-    }
   }
 }
 
