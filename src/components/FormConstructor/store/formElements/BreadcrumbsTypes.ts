e78996d48d0fdb4ba0f1f48c4f9822423495fccb
Andrew161644 <truba24032016@mail.ru>
2023-07-08 23:38:47 +0500

    Добавить отображение выбранной страницы

--- src/components/FormConstructor/store/formElements/BreadcrumbsTypes.ts
+++ src/components/FormConstructor/store/formElements/BreadcrumbsTypes.ts
@@ -1,6 +1,4 @@
-import { IconPropSize } from '@consta/uikit/Icon'
 import { BaseProps, IFormElement } from './types'
-import { TabsPropFitMode } from '@consta/uikit/Tabs'
 import { BreadcrumbsProps } from '@consta/uikit/Breadcrumbs'
 
 export type BreadcrumbProps = BreadcrumbsProps & BaseProps
