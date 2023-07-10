5153ed35535f1664de12dcfc14b61f85afb9bbc9
Andrew161644 <truba24032016@mail.ru>
2023-07-10 10:42:12 +0500

    Добавить компонент палитры
    mend

--- src/pages/FormConstructorPage/index.tsx
+++ src/pages/FormConstructorPage/index.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
-
+import { FormConstructor } from '../../components/FormConstructor'
 import styles from './styles.module.css'
-import { ConstaColor, ConstaPalette } from '../../components/ConstaPalette'
 
 export const FormConstructorPage = () => {
-  const [constaColor, setConstaColor] = useState<ConstaColor>('color-bg-link')
   return (
     <div className={styles.mainPage}>
-      <ConstaPalette
-        color={constaColor}
-        size='m'
-        onChangeColor={color => {
-          setConstaColor(color)
-        }}
-      />
+      <FormConstructor />
     </div>
   )
 }
