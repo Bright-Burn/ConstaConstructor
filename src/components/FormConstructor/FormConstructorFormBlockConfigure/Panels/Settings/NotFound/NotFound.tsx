e086b5190e475b639185fa9120c63631286843ae
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-08-04 10:00:49 +0500

    fix: Поправить код

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/NotFound/NotFound.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/NotFound/NotFound.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { Text } from '@consta/uikit/Text'
 import style from './styles.module.css'
 import { ResponsesEmptyPockets } from '@consta/uikit/ResponsesEmptyPockets'
 
 export const NotFound = () => {
   return (
     <>
-      <div className={style.paddingSvg}>
-        <div className={style.notFound}>
-          <ResponsesEmptyPockets
-            title=' '
-            description='Нет выбранного элемента для настройки'
-            actions={<></>}
-            size='m'
-          />
-        </div>
-      </div>
+      <ResponsesEmptyPockets
+        title=' '
+        description='Нет выбранного элемента для настройки'
+        actions={<></>}
+        size='m'
+      />
     </>
   )
 }
