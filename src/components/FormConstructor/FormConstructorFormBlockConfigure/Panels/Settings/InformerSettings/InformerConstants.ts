333d72506d47939e2bff3af009f12979e4589ebc
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-08-08 11:09:08 +0500

    fix: Убрать ts-ignore и подправить отображние в настройках информера

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/InformerSettings/InformerConstants.ts
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/InformerSettings/InformerConstants.ts
@@ -2,4 +2,4 @@ import { InformerPropSize, InformerPropView, InformerPropStatus } from '@consta/
 
 export const sizes: InformerPropSize[] = ['m', 's']
 export const views: InformerPropView[] = ['filled', 'bordered']
-export const statuses: InformerPropStatus[] = ['success', 'system', 'alert', 'warning']
+export const statuses: InformerPropStatus[] = ['system', 'success', 'alert', 'warning']
