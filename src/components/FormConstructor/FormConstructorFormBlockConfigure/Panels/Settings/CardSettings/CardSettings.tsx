aa9bc763a72d236d1eec81382c9f28b9e837270b
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-08-08 10:19:38 +0500

    fix: Обновить дизайн настроек карточки

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/CardSettings/CardSettings.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/CardSettings/CardSettings.tsx
@@ -7,6 +7,8 @@ import { TextField } from '@consta/uikit/TextField'
 import { ISelectedElement } from '../../../../coreTypes'
 import { setSelectedElement, useAppDispatch } from '../../../../store'
 import { BrandCardElementPropsStyles, CardElement } from '../../../../coreTypes/cardTypes'
+import { ChoiceGroup } from '@consta/uikit/ChoiceGroup'
+import { Text } from '@consta/uikit/Text'
 
 type CardSettingsType = {
   selectedElementProps: CardElementPropsStyles
@@ -17,7 +19,6 @@ export const CardSettings: FC<CardSettingsType> = ({ selectedElementProps, selec
   const [props, setProps] = useState<CardElementPropsStyles>()
   const status: string[] = ['alert', 'success', 'warning', 'undefined']
   const form: string[] = ['round', 'square']
-  const space: string[] = ['m', 'xs', 's', 'l', 'xl', '2xl', '3xl', '4xl', '5xl']
   const [widthValue, setWidthValue] = useState<string>('376')
   const [heightValue, setHeightValue] = useState<string>('227')
 
@@ -44,9 +45,7 @@ export const CardSettings: FC<CardSettingsType> = ({ selectedElementProps, selec
         props: { ...selectedElementProps },
         type: 'Card',
       }
-      newProps.props.constaProps = { ...newProps.props.constaProps }
-      // @ts-ignore
-      newProps.props.constaProps[propsName] = value
+      newProps.props.constaProps = { ...newProps.props.constaProps, [propsName]: value }
       onDispatch(selectedElement, newProps)
     }
 
@@ -57,9 +56,12 @@ export const CardSettings: FC<CardSettingsType> = ({ selectedElementProps, selec
         props: { ...selectedElementProps },
         type: 'Card',
       }
-      newProps.props.constaProps = { ...newProps.props.constaProps }
-      // @ts-ignore
-      newProps.props.constaProps[propsName] = checked
+      newProps.props.constaProps = { ...newProps.props.constaProps, [propsName]: checked }
+
+      if (propsName === 'border' && checked === false) {
+        newProps.props.constaProps.status = undefined
+      }
+
       onDispatch(selectedElement, newProps)
     }
 
@@ -107,61 +109,64 @@ export const CardSettings: FC<CardSettingsType> = ({ selectedElementProps, selec
     <div className={styles.cardSettings}>
       {props ? (
         <>
-          <Select
-            getItemKey={key => key}
-            label='VerticalSpace'
-            getItemLabel={label => label}
-            items={space}
-            value={`${props.constaProps.verticalSpace}`}
-            onChange={onChangeCardField('verticalSpace')}
-          />
-          <Select
-            getItemKey={key => key}
-            label='HorizontalSpace'
-            getItemLabel={label => label}
-            items={space}
-            value={`${props.constaProps.horizontalSpace}`}
-            onChange={onChangeCardField('horizontalSpace')}
-          />
-          <Select
-            getItemKey={key => key}
-            label='Status'
-            getItemLabel={label => label}
-            items={status}
-            value={`${props.constaProps.status}`}
-            onChange={onChangeCardField('status')}
-          />
-          <Select
-            getItemKey={key => key}
-            label='Form'
-            getItemLabel={label => label}
-            items={form}
-            value={`${props.constaProps.form}`}
-            onChange={onChangeCardField('form')}
-          />
+          <div className={styles.rowSettings}>
+            <TextField
+              onChange={({ value }: { value: string | null }) => onChangeHeight(value)}
+              value={heightValue}
+              type='number'
+              size='xs'
+              leftSide='px'
+              label='Высота'
+              min='0'
+            />
+            <TextField
+              onChange={({ value }: { value: string | null }) => onChangeWidth(value)}
+              value={widthValue}
+              type='number'
+              size='xs'
+              leftSide='px'
+              label='Ширина'
+              min='0'
+            />
+          </div>
+          <div className={styles.rowSettings}>
+            <div className={styles.columnSettings}>
+              <Text view='secondary' size='xs'>
+                Форма углов
+              </Text>
+              <ChoiceGroup
+                value={props.constaProps.form}
+                onChange={onChangeCardField('form')}
+                items={form}
+                size='xs'
+                view='ghost'
+                getItemLabel={item => item}
+                name='ChoiceGroupExample'
+              />
+            </div>
+            <div className={styles.columnSettings}>
+              <Switch
+                checked={props.constaProps.border}
+                label='Границы'
+                size='xs'
+                onChange={onChangeCardSwitch('border')}
+              />
+              <Select
+                getItemKey={key => key}
+                size='xs'
+                disabled={!props.constaProps.border}
+                getItemLabel={label => label}
+                items={status}
+                value={`${props.constaProps.status}`}
+                onChange={onChangeCardField('status')}
+              />
+            </div>
+          </div>
           <Switch
             checked={props.constaProps.shadow ?? true}
-            label='shadow'
+            label='Тень'
+            size='xs'
             onChange={onChangeCardSwitch('shadow')}
-          />
-          <Switch
-            checked={props.constaProps.border ?? false}
-            label='border'
-            onChange={onChangeCardSwitch('border')}
-          />
-          <TextField
-            onChange={({ value }: { value: string | null }) => onChangeWidth(value)}
-            value={widthValue}
-            type='number'
-            label='Width'
-            min='0'
-          />
-          <TextField
-            onChange={({ value }: { value: string | null }) => onChangeHeight(value)}
-            value={heightValue}
-            type='number'
-            label='Height'
-            min='0'
           />
         </>
       ) : (
