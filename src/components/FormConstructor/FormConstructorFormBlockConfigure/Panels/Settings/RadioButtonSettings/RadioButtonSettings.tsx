aff2e4ce6dd2ff683be94546ea7a6ebbbbe9051a
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-08-09 12:05:27 +0500

    style: Обновить дизайн настроек радиокнопки

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/RadioButtonSettings/RadioButtonSettings.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/Settings/RadioButtonSettings/RadioButtonSettings.tsx
@@ -7,6 +7,9 @@ import { Switch } from '@consta/uikit/Switch'
 import { FC } from 'react'
 import { RadioButtonProps } from '../../../../coreTypes'
 import { RadioButtonElement } from '../../../../coreTypes/radioButtonTypes'
+import styles from './styles.module.css'
+import { Text } from '@consta/uikit/Text'
+import { ChoiceGroup } from '@consta/uikit/ChoiceGroup'
 
 type RadioButtonSettingsType = {
   selectedElementProps: RadioButtonProps
@@ -22,41 +25,66 @@ export const RadioButtonSettings: FC<RadioButtonSettingsType> = ({
 
   return (
     <>
-      <Switch
-        checked={itemsProps.disabled ?? false}
-        label='disabled'
-        onChange={onChangeSwitch('disabled')}
-      />
-      <Switch
-        checked={itemsProps.checked ?? false}
-        label='checked'
-        onChange={onChangeSwitch('checked')}
-      />
-      <Select
-        label='size'
-        getItemKey={(key: RadioPropSize) => key}
-        getItemLabel={(label: RadioPropSize) => label}
-        value={itemsProps.size}
-        items={sizeArray}
-        onChange={({ value }) => onChangeSize(value)}
-      />
-      <Select
-        label='view'
-        getItemKey={(key: RadioPropView) => key}
-        getItemLabel={(label: RadioPropView) => label}
-        value={itemsProps.view}
-        items={viewArray}
-        onChange={({ value }) => onChangeView(value)}
-      />
-      <Select
-        label='align'
-        getItemKey={(key: RadioPropAlign) => key}
-        getItemLabel={(label: RadioPropAlign) => label}
-        value={itemsProps.align}
-        items={alignArray}
-        onChange={({ value }) => onChangeAlign(value)}
-      />
-      <TextField label='label' value={itemsProps.label} onChange={onChangeField('label')} />
+      <div className={styles.radioButtonSettings}>
+        <TextField
+          size='xs'
+          label='Текст'
+          value={itemsProps.label}
+          onChange={onChangeField('label')}
+        />
+        <div className={styles.rowSettings}>
+          <Select
+            className={styles.widthFlex}
+            label='Размер'
+            size='xs'
+            getItemKey={(key: RadioPropSize) => key}
+            getItemLabel={(label: RadioPropSize) => label}
+            value={itemsProps.size}
+            items={sizeArray}
+            onChange={({ value }) => onChangeSize(value)}
+          />
+          <div className={styles.columnInRowSettings}>
+            <Text size='xs' view='secondary'>
+              Вид
+            </Text>
+            <ChoiceGroup
+              value={itemsProps.view}
+              items={viewArray}
+              size='xs'
+              view='ghost'
+              getItemLabel={(label: RadioPropView) => label}
+              name='ChoiceGroupExample'
+              onChange={({ value }) => onChangeView(value)}
+            />
+          </div>
+        </div>
+        <div className={styles.columnSettings}>
+          <Text size='xs' view='secondary'>
+            Выравнивание
+          </Text>
+          <ChoiceGroup
+            value={itemsProps.align}
+            items={alignArray}
+            size='xs'
+            view='ghost'
+            getItemLabel={(label: RadioPropAlign) => label}
+            name='ChoiceGroupExample'
+            onChange={({ value }) => onChangeAlign(value)}
+          />
+        </div>
+        <Switch
+          checked={itemsProps.checked}
+          label='Активен'
+          size='xs'
+          onChange={onChangeSwitch('checked')}
+        />
+        <Switch
+          checked={itemsProps.disabled}
+          label='Состояние блокировки'
+          size='xs'
+          onChange={onChangeSwitch('disabled')}
+        />
+      </div>
     </>
   )
 }
