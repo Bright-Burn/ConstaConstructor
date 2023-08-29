d7e521f6d9f1450fabe25f0d5fc238711ca6241b
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-08-29 13:52:01 +0500

    refactor: Вынести pageName и создать PageEdit

---
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/FormBlock/Pages/PageEdit/PageEdit.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@consta/uikit/Button'
+import { FC, useState } from 'react'
+import { IPageEdit } from './types'
+import { TextField } from '@consta/uikit/TextField'
+import { IconCheck } from '@consta/icons/IconCheck'
+
+export const PageEdit: FC<IPageEdit> = ({ newPageName, isSelectedPage, defaultPageName }) => {
+  const [pageName, setPageName] = useState<string | null>(defaultPageName)
+
+  const changePageName = ({ value }: { value: string | null }) => setPageName(value)
+
+  return (
+    <>
+      <TextField size='xs' type='text' value={pageName} onChange={changePageName}></TextField>
+      <Button
+        iconLeft={IconCheck}
+        view={isSelectedPage ? 'ghost' : 'clear'}
+        size='xs'
+        form='brick'
+        onlyIcon
+        onClick={() => newPageName(pageName)}
+      />
+    </>
+  )
+}
---
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/FormBlock/Pages/PageEdit/index.ts
@@ -0,0 +1,1 @@
+export { PageEdit } from './PageEdit'
