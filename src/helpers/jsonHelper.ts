8076e58da898db00f57d2aa7c60ac424c86cce2f
Andrew161644 <truba24032016@mail.ru>
2023-10-08 17:46:45 +0500

    Перенести DATE_REGEXP в функцию

--- src/helpers/jsonHelper.ts
+++ src/helpers/jsonHelper.ts
@@ -1,5 +1,3 @@
-const DATE_REGEXP = /^\d{4}[.\/-]\d{2}[.\/-]\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/g
-
 export class JsonHelper {
   public static parse(data: string): any {
     return JSON.parse(data, JsonHelper.CustomStringParser)
@@ -8,8 +6,9 @@ export class JsonHelper {
   private static CustomStringParser(key: string, value: any): any {
     if (typeof value === 'string') {
       /// Check if date fortmat
-      let a = DATE_REGEXP.exec(value)
-      if (a) {
+      const DATE_REGEXP = /^\d{4}[.\/-]\d{2}[.\/-]\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/g
+
+      if (DATE_REGEXP.test(value)) {
         return new Date(value)
       }
     }
