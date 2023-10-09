92443d82514e4bfc220b144c0f44f2679f64ab15
Andrew161644 <truba24032016@mail.ru>
2023-10-09 12:48:09 +0500

    Добавить определение LayoutElement

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/DroppableLocalLayer/types.ts
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/DroppableLocalLayer/types.ts
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
+import { FormElementTypes, FormGroupsTypes } from '../../coreTypes'
 
 export interface IDroppableLocalLayer {
   children?: ReactNode
   parentElementId: string
+  isLayout: boolean
 }
