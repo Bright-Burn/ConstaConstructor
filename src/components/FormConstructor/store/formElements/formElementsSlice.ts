aa9c4b1a6217bc128a31ca5d67d413234a6bdba9
Шарафутдинов Ильгиз <ilgiz93sharafutdinov@gmail.com>
2024-03-15 12:12:48 +0500

    Исправлены ошибки в типах

--- src/components/FormConstructor/store/formElements/formElementsSlice.ts
+++ src/components/FormConstructor/store/formElements/formElementsSlice.ts
@@ -15,20 +15,34 @@ import { initialState } from './initialState'
 import { popHistory } from './history/reducers'
 import { deselectElement } from './reducers/setSelectedElement'
 
+//TODO необходимо логику из редьюсеров перенести в экшены, а редьюсеры в слайсы
+// иначе не может привести к типу WriteableDraft<IFormConstructor>
+// @ts-ignore
 export const formConstructorSlice = createSlice({
   name: 'formConstructor',
   initialState,
   reducers: {
+    // @ts-ignore
     setDraggableElement: setDraggableElement,
+    // @ts-ignore
     loadProjectFromJson: loadProjectFromJson,
+    // @ts-ignore
     setSelectedElement: setSelectedElement,
+    // @ts-ignore
     deselectElement: deselectElement,
+    // @ts-ignore
     addNewFormElement: addNewFormElement,
+    // @ts-ignore
     deleteFormElement: deleteFormElement,
+    // @ts-ignore
     addNewPage: addNewPage,
+    // @ts-ignore
     changeActivePage: changeActivePage,
+    // @ts-ignore
     changePageName: changePageName,
+    // @ts-ignore
     deletePage: deletePage,
+    // @ts-ignore
     popHistory: popHistory,
   },
 })
