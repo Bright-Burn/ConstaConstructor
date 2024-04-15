50b634890fbc0d1fa86aa5b817e2487a79b6a1f5
Шарафутдинов Ильгиз <ilgiz93sharafutdinov@gmail.com>
2024-04-15 20:41:27 +0500

    Добавлен механизм раскрытия дерева элементов при выборе компонентов

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/ComponentsStructure/ComponentTree/Tree/Tree.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/ComponentsStructure/ComponentTree/Tree/Tree.tsx
@@ -1,8 +1,10 @@
 import type { FC } from 'react'
+import { useEffect, useState } from 'react'
 import { cnRcTree } from '@consta/rc-tree-adapter/RcTree'
 import { rcTreeAdapter } from '@consta/rc-tree-adapter/rcTreeAdapter'
 import RCTree from 'rc-tree'
 
+import type { IFormElement, IGroupElement } from '../../../../../coreTypes'
 import {
   getFormElAsMap,
   setSelectedElement,
@@ -16,6 +18,7 @@ export const Tree: FC<ITree> = ({ data }) => {
   const allElementsMap = useAppSelector(getFormElAsMap)
   const selectedEl = useAppSelector(state => state.formConstructor.selectedElement)
   const dispatch = useAppDispatch()
+  const [expandedKeys, setExpandedKeys] = useState<(string | number)[]>([])
 
   const treeProps = rcTreeAdapter()
   const prefix = cnRcTree(
@@ -24,6 +27,15 @@ export const Tree: FC<ITree> = ({ data }) => {
     },
     ['CustomTree'],
   )
+  useEffect(() => {
+    if (selectedEl) {
+      //TODO раскоментировать если есть необходимость не закрывать предыдущие узлы когда выбираются новые
+      // const parentsIds = getParentsIds(allElementsMap, [], selectedEl.elementId)
+      //       const expandedKeysSet = new Set([...expandedKeys, ...parentsIds])
+      //       setExpandedKeys(Array.from(expandedKeysSet))
+      setExpandedKeys(getParentsIds(allElementsMap, [], selectedEl.elementId))
+    }
+  }, [selectedEl])
 
   const onSelect = (selectedKeys: (string | number)[]) => {
     selectedKeys.forEach(key => {
@@ -38,15 +50,31 @@ export const Tree: FC<ITree> = ({ data }) => {
       }
     })
   }
-
+  const onExpand = (keys: (string | number)[]) => {
+    setExpandedKeys(keys)
+  }
   return (
     <RCTree
       {...treeProps}
       treeData={data}
       prefixCls={prefix}
       selectedKeys={[selectedEl?.elementId ?? '']}
+      expandedKeys={expandedKeys}
       defaultExpandAll={true}
       onSelect={onSelect}
+      onExpand={onExpand}
     />
   )
+}
+
+const getParentsIds = (
+  allElems: Map<string, IFormElement | IGroupElement>,
+  elemsArr: string[],
+  elemId: string,
+): string[] => {
+  const parentId = allElems.get(elemId)?.parentId
+  if (!parentId) {
+    return elemsArr
+  }
+  return getParentsIds(allElems, [...elemsArr, parentId], parentId)
 }
