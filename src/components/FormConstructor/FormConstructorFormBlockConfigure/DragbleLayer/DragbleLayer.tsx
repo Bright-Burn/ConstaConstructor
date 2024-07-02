4cf87c57504581bc6aace3b8e809ed97d21afc3c
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2024-07-02 18:57:39 +0500

    Изменен способ пересортировки элементов

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/DragbleLayer/DragbleLayer.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/DragbleLayer/DragbleLayer.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import { reorderFormElement, useAppDispatch, useAppSelector } from '../../store'
 
@@ -7,28 +7,22 @@ import type { IDragbleleLayer } from './types'
 
 import css from './styles.module.css'
 export const DragbleLayer: FC<IDragbleleLayer> = ({ children, className, elId }) => {
-  const [isDragging, setIsDragging] = useState(false)
+  const [isDragLeft, setIsDragLeft] = useState(false)
+  const [isDragRight, setIsDragRight] = useState(false)
   const dispatch = useAppDispatch()
   const selectedEl = useAppSelector(state => state.formConstructor.selectedElement)
-
   const onDragStart = (event: React.DragEvent) => {
     event.stopPropagation()
     event.dataTransfer.setData('draggedElId', elId)
   }
-  const onDragEnter = (event: React.DragEvent) => {
-    setIsDragging(true)
-    event.stopPropagation()
-  }
-  const onDragEnd = () => {
-    setIsDragging(false)
-  }
+
   const onDragLeave = () => {
-    setIsDragging(false)
+    setIsDragLeft(false)
+    setIsDragRight(false)
   }
   const onDrop = (event: React.DragEvent, left: boolean) => {
-    setIsDragging(false)
+    setIsDragLeft(false)
     const draggedELId = event.dataTransfer.getData('draggedElId')
-    console.log('L31 drop ===', 'drop')
     dispatch(
       reorderFormElement({
         elementId: draggedELId,
@@ -44,26 +38,44 @@ export const DragbleLayer: FC<IDragbleleLayer> = ({ children, className, elId })
   const onDropRight = (event: React.DragEvent) => {
     onDrop(event, false)
   }
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.stopPropagation()
+    event.preventDefault()
+    const target = event.target as HTMLElement
+    const nativeEvent = event.nativeEvent
+    const layerX = nativeEvent.offsetX
+
+    if (layerX < target.offsetWidth / 2) {
+      setIsDragRight(false)
+      setIsDragLeft(true)
+    } else {
+      setIsDragRight(true)
+      setIsDragLeft(false)
+    }
+  }
+  const onDropToDragbleLayer = (event: React.DragEvent) => {
+    event.stopPropagation()
+    onDragLeave()
+    const target = event.target as HTMLElement
+    const nativeEvent = event.nativeEvent
+    const layerX = nativeEvent.layerX
+    if (layerX < 8) {
+      onDropLeft(event)
+      console.log('left drop')
+    } else if (layerX > target.offsetWidth - 8) {
+      onDropRight(event)
+      console.log('right drop')
+    }
+  }
   return (
     <div
-      className={`${className} ${css.dragbleContainer}`}
+      className={`${className} ${css.dragbleContainer} ${isDragLeft ? css.draggingLeft : ''} ${isDragRight ? css.draggingRight : ''}`}
       draggable={true}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDropToDragbleLayer}
       onDragStart={onDragStart}>
-      <div
-        className={`${css.right} ${isDragging ? css.dragging : ''} `}
-        onDrop={onDropRight}
-        onDragEnd={onDragEnd}
-        onDragLeave={onDragLeave}
-        onDragEnter={onDragEnter}
-      />
-      <div
-        className={`${css.left} ${isDragging ? css.dragging : ''} `}
-        onDragEnd={onDragEnd}
-        onDragLeave={onDragLeave}
-        onDragEnter={onDragEnter}
-        onDrop={onDropLeft}
-      />{' '}
-      {children}{' '}
+      {children}
     </div>
   )
 }
