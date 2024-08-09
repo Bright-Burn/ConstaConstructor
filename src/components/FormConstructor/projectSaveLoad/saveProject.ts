b9af21d9b59d935462b9aea935117a1acabea2b1
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2024-08-09 15:53:13 +0500

    Добавлена выгрузка в ZIP архив

--- src/components/FormConstructor/projectSaveLoad/saveProject.ts
+++ src/components/FormConstructor/projectSaveLoad/saveProject.ts
@@ -57,7 +57,7 @@ export const saveProjectToHtml = (projData: SaveProjectIntent) => {
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Document</title>
-          <script async defer src='${document.scripts[0].src}'></script>
+          <script async defer src='./script.js'></script>
         </head>
         <body>
         <div class='Theme Theme_color_gpnDefault Theme_control_gpnDefault Theme_font_gpnDefault Theme_size_gpnDefault Theme_space_gpnDefault Theme_shadow_gpnDefault'>
@@ -66,12 +66,32 @@ export const saveProjectToHtml = (projData: SaveProjectIntent) => {
         <div style='display: none' id='loaded_data'>${JSON.stringify(projData)}</div>
         </body>
         </html>`
-  Promise.all([css, js]).then(([cssText]) => {
-    const blob = new Blob([html, `<style>${cssText}</style> `], { type: 'text/html' })
-    const url = URL.createObjectURL(blob)
-    const tagA = document.createElement('a')
-    tagA.href = url
-    tagA.download = 'webpage.html'
-    tagA.click()
+
+  Promise.all([css, js]).then(([cssText, jsText]) => {
+    //@ts-expect-error библиотека подключена через jszip.min.js
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const zip = new JSZip()
+
+    // Добавляем HTML файл
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    zip.file('index.html', `${html}<style>${cssText}</style>`)
+
+    // Добавляем JavaScript файл
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    zip.file('script.js', jsText)
+
+    // Генерируем ZIP файл
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    zip.generateAsync({ type: 'blob' }).then((content: Blob | MediaSource) => {
+      // Создаем ссылку для скачивания ZIP файла
+      const url = URL.createObjectURL(content)
+      const tagA = document.createElement('a')
+      tagA.href = url
+      tagA.download = `${projData.name}.zip`
+      tagA.click()
+
+      // Освобождаем URL
+      URL.revokeObjectURL(url)
+    })
   })
 }
