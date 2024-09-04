ec5b99352343a37f61cdbcfb00a78d54ebc6a5c5
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2024-09-04 15:00:55 +0500

    Добавлены иконки

---
+++ src/components/FormConstructor/coreTypes/icons/human.ts
@@ -0,0 +1,13 @@
+import { IconOperators } from '@consta/icons/IconOperators'
+import { IconSmileFilled } from '@consta/icons/IconSmileFilled'
+import { IconSmileStroked } from '@consta/icons/IconSmileStroked'
+import { IconTeam } from '@consta/icons/IconTeam'
+import { IconUser } from '@consta/icons/IconUser'
+
+export const HumanIcons = {
+  IconOperators,
+  IconSmileFilled,
+  IconSmileStroked,
+  IconTeam,
+  IconUser,
+}
---
+++ src/components/FormConstructor/coreTypes/icons/tools.ts
@@ -0,0 +1,37 @@
+import { IconToolsApproach } from '@consta/icons/IconToolsApproach'
+import { IconToolsBuildTurn } from '@consta/icons/IconToolsBuildTurn'
+import { IconToolsCurveHold } from '@consta/icons/IconToolsCurveHold'
+import { IconToolsDiameter } from '@consta/icons/IconToolsDiameter'
+import { IconToolsDiameterInner } from '@consta/icons/IconToolsDiameterInner'
+import { IconToolsDiameterOuter } from '@consta/icons/IconToolsDiameterOuter'
+import { IconToolsDogled } from '@consta/icons/IconToolsDogled'
+import { IconToolsHold } from '@consta/icons/IconToolsHold'
+import { IconToolsHoldCurveHold } from '@consta/icons/IconToolsHoldCurveHold'
+import { IconToolsHoldFin } from '@consta/icons/IconToolsHoldFin'
+import { IconToolsHoldMid } from '@consta/icons/IconToolsHoldMid'
+import { IconToolsHoldStart } from '@consta/icons/IconToolsHoldStart'
+import { IconToolsIncline } from '@consta/icons/IconToolsIncline'
+import { IconToolsLeftDown } from '@consta/icons/IconToolsLeftDown'
+import { IconToolsMaxAngle } from '@consta/icons/IconToolsMaxAngle'
+import { IconToolsRightTop } from '@consta/icons/IconToolsRightTop'
+import { IconToolsSWell } from '@consta/icons/IconToolsSWell'
+
+export const ToolsIcon = {
+  IconToolsApproach,
+  IconToolsBuildTurn,
+  IconToolsCurveHold,
+  IconToolsDiameter,
+  IconToolsDiameterInner,
+  IconToolsDiameterOuter,
+  IconToolsDogled,
+  IconToolsHold,
+  IconToolsHoldCurveHold,
+  IconToolsHoldFin,
+  IconToolsHoldMid,
+  IconToolsHoldStart,
+  IconToolsIncline,
+  IconToolsLeftDown,
+  IconToolsMaxAngle,
+  IconToolsRightTop,
+  IconToolsSWell,
+}
