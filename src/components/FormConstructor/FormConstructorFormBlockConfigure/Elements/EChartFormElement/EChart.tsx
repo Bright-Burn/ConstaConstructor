b7ea76fa77656c03001649f00a220ec1bbac7718
Шарафутдинов Ильгиз <ilgiz93sharafutdinov@gmail.com>
2024-09-05 14:49:56 +0500

    Добавлена темизация для Echarts

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Elements/EChartFormElement/EChart.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Elements/EChartFormElement/EChart.tsx
@@ -1,49 +1,55 @@
 import type { FC } from 'react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import type { EChartsType } from 'echarts'
 import { init, registerTheme } from 'echarts'
 
 import { ElementTypes, FormElementDictTypes } from '../../../coreTypes'
 import { formInstancePropsSelector, useAppSelector } from '../../../store'
+import { useTheme } from '../../../utils'
 import { SelectableLayer } from '../../SelectableLayer'
 
+import { darkTheme } from './darkTheme'
 import { defaultTheme } from './defaultTheme'
 import type { IEChartFormElement } from './types'
 //регисрация темы для чарта
-registerTheme('default', defaultTheme)
 
 export const EChartFormElement: FC<IEChartFormElement> = ({ element }) => {
+  const theme = useTheme()
   const props = useAppSelector(formInstancePropsSelector(element.instanceId, element.type))?.props
   const width = props?.width
   const height = props?.height
   const ref = React.useRef(null)
   const chartRef = React.useRef<EChartsType | null>(null)
-  useEffect(() => {
-    //TODO при смене темы надо прописывать название в init
-    const myChart = init(ref.current, 'default')
+
+  useLayoutEffect(() => {
+    if (theme === 'light') registerTheme('light', defaultTheme)
+    else registerTheme('dark', darkTheme)
+
+    chartRef.current?.dispose()
+    const myChart = init(ref.current, theme)
     chartRef.current = myChart
-    myChart.setOption({
-      title: {
-        text: 'ECharts Getting Started Example',
-      },
-      tooltip: {},
-      xAxis: {
-        data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks', 'box'],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: 'sales',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20, 30],
+    if (props.options === '') {
+      myChart.setOption({
+        title: {
+          text: 'ECharts Getting Started Example',
         },
-      ],
-    })
-    myChart.resize({
-      width: 400,
-      height: 400,
-    })
-  }, [])
+        tooltip: {},
+        xAxis: {
+          data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks', 'box'],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20, 30],
+          },
+        ],
+      })
+    } else {
+      setNewOptions(props.options)
+    }
+  }, [theme])
 
   useEffect(() => {
     chartRef.current?.resize({
@@ -51,18 +57,23 @@ export const EChartFormElement: FC<IEChartFormElement> = ({ element }) => {
       height: props?.height ?? 400,
     })
   }, [props?.height, props?.width])
+
   useEffect(() => {
     if (props?.options) {
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const options = JSON.parse(props.options)
-        chartRef.current?.clear()
-        chartRef.current?.setOption(options)
-      } catch (e) {
-        console.log('L61  === невалидный Options в Echart')
-      }
+      setNewOptions(props.options)
     }
   }, [props?.options])
+
+  const setNewOptions = (options: string) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const options = JSON.parse(props.options)
+      chartRef.current?.clear()
+      chartRef.current?.setOption(options)
+    } catch (e) {
+      console.log('L61  === невалидный Options в Echart')
+    }
+  }
   return (
     <SelectableLayer
       parentElementId={element.id}
