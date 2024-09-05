90aa4c5886c05a9499f05dc78c4454148600041e
Andrew161644 <truba24032016@mail.ru>
2024-09-05 10:00:33 +0500

    Добавить текстовые поля

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/CodeText.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/CodeText.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react'
 import { CodeTextProps } from './types'
+import { Text } from '@consta/uikit/Text'
+import styles from './styles.module.css'
 
-export const CodeText: FC<CodeTextProps> = ({ text }) => {
-  return <div>{text}</div>
+export const CodeText: FC<CodeTextProps> = ({ text, label }) => {
+  return text.length ? (
+    <Text size="s" className={`p-l-xs p-r-xs p-b-xs p-t-xs ${styles.code}`}>
+      {text}
+    </Text>
+  ) : null
 }
--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/DeveloperPanel.tsx
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/DeveloperPanel.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
 import { CodeText } from './CodeText'
+import styles from './styles.module.css'
 
 export const DeveloperPanel: FC = () => {
-  
   return (
-    <div>
+    <div className={`container-column p-r-xs flex-grow-1 ${styles.code_pane}`}>
       <CodeText text="" label="Стили" />
-      <CodeText text="" label="Код" />
+      <CodeText text="" label="Стили" />
     </div>
   )
 }
