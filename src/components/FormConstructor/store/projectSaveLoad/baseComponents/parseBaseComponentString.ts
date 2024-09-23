b8c4b0a722fb375fc43f96213b7e98b9a16a0d36
Andrew161644 <truba24032016@mail.ru>
2024-09-23 11:31:19 +0500

    Добавить описания

--- src/components/FormConstructor/store/projectSaveLoad/baseComponents/parseBaseComponentString.ts
+++ src/components/FormConstructor/store/projectSaveLoad/baseComponents/parseBaseComponentString.ts
@@ -1,8 +1,14 @@
 import { JsonHelper } from '../../../../../helpers'
 import { BaseComponentToSave } from '../types'
 
+/**
+ * Парсит строку в промежуточную структуру сериализуеммого базового компонента
+ * @param projectString Json Строка
+ * @returns Промежуточную структура сериализуеммого базового компонента
+ */
 export const parseBaseComponentString = (json: string) => {
-  // Добавить проверку типов
+  //TODO надо сделать проверку рантайм, что файл соответствует нашему контракту!
   const baseComponent: BaseComponentToSave = JsonHelper.parse(json)
+  // Здесь будет использоваться адаптер для поддержания версии выгрузки проекта для переезда на новые пропсы при необходимости
   return baseComponent
 }
--- src/components/FormConstructor/store/projectSaveLoad/project/parseProjectString.ts
+++ src/components/FormConstructor/store/projectSaveLoad/project/parseProjectString.ts
@@ -1,9 +1,15 @@
 import { JsonHelper } from '../../../../../helpers'
 import { FormConstructorToSave } from '../types'
 
+/**
+ * Парсит строку в промежуточную структуру сериализуеммого проекта
+ * @param projectString Json Строка
+ * @returns Промежуточную структура сериализуеммого проекта
+ */
 export const parseProjectString = (projectString: string) => {
   //TODO надо сделать проверку рантайм, что файл соответствует нашему контракту!
   const parsedFile = JsonHelper.parse(projectString)
+  // Здесь будет использоваться адаптер для поддержания версии проекта для переезда на новые пропсы
   const project = parsedFile.project as FormConstructorToSave
   return project
 }
