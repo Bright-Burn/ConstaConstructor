0ca0b2f0e723c80ebb305171b88829a4e604a6af
Andrew161644 <truba24032016@mail.ru>
2024-09-23 17:43:10 +0500

    Добавить обработку иконок при генерации кода"

--- src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/CodeBuilder/buildConstaPropsCommon.ts
+++ src/components/FormConstructor/FormConstructorFormBlockConfigure/Panels/DeveloperPanel/CodeBuilder/buildConstaPropsCommon.ts
@@ -1,5 +1,8 @@
 export type ConstaPropsStyles = Record<string, string | number | boolean | object>
 
+// Нименования свойств в значении которых может быть иконка начинаются с Icon
+const IconPrefix = 'icon'
+
 /**
  * Строит стили из дизайн системы для элемента
  * @param obj Оъект стилей
@@ -8,7 +11,7 @@ export type ConstaPropsStyles = Record<string, string | number | boolean | objec
 export const buildConstaPropsCommon = (props: ConstaPropsStyles): string => {
   let resultString = ''
   Object.entries(props).forEach(([key, value]) => {
-    typeof value !== 'string'
+    typeof value !== 'string' || key.startsWith(IconPrefix)
       ? (resultString += `${key}={${value}}\n`)
       : (resultString += `${key}={'${value}'}\n`)
   })
